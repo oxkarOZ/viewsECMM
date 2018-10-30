8de123111768d58e6304f62bae74ce00a789822c
oscar <oscar_tavarez@live.com.mx>
2018-10-29 21:11:28 -0600

    Se agrega la funcionalidad de slideProducts apuntando al servicio de featured

--- js/index/indexCoco.js
+++ js/index/indexCoco.js
@@ -1,8 +1,12 @@
 var featuredProducts;
-var cards = $();
+var slideProducts;
+var cardsFeatured = $();
+var cardsSlide = $();
+
 
 $(function() {
 	cargaProductos();
+	cargaSlide();
 });
 
 
@@ -14,9 +18,9 @@ function cargaProductos(){
 		success: function(respuesta) {
 			featuredProducts = respuesta;
 			featuredProducts.forEach(function(item, i) {
-			  cards = cards.add(loadFeatured(item));
+			  cardsFeatured = cardsFeatured.add(loadFeatured(item));
 			});
-			$('#rowFeatured').append(cards);
+			$('#rowFeatured').append(cardsFeatured);
 		},
 		error: function() {
 	        console.log("No se ha podido obtener la información");
@@ -40,3 +44,54 @@ function loadFeatured(cardData){
 	return $(cardFeaturedTemplate.join(''));
 }
 
+
+
+
+function cargaSlide(){
+	$.ajax({
+		type: "GET",
+		url: 'http://localhost:8080/featured/all',
+		success: function(respuesta) {
+			slideProducts = respuesta;
+			slideProducts.forEach(function(item, i) {
+			  cardsSlide = cardsSlide.add(loadSlide(item));
+			});
+			$('#divSlide').append(cardsSlide);
+			var owl = $('#divSlide');
+			owl.addClass("owl-carousel large-controls dots-inside");
+
+			owl.owlCarousel({
+				items:1,
+			    autoHeight:true,
+			    loop:true,
+			    autoplay:true,
+			    autoplayTimeout:3000,
+			    nav:true,
+			    dots:true,
+			    navText:["", ""]
+			});
+		},
+		error: function() {
+	        console.log("No se ha podido obtener la información");
+	    }
+	});
+}
+
+function loadSlide(cardData){
+	var cardFeaturedTemplate = [
+		'<div class="item">',
+          '<div class="container padding-top-3x">',
+            '<div class="row justify-content-center align-items-center">',
+              '<div class="col-lg-5 col-md-6 padding-bottom-2x text-md-left text-center">',
+                '<div class="from-bottom"><img class="d-inline-block w-150 mb-4" src="img/hero-slider/logoRev.jpg" alt="Puma">',
+                  '<div class="h2 text-body mb-2 pt-1">'+cardData.category.categoryName+' - '+cardData.productName+'</div>',
+                  '<div class="h2 text-body mb-4 pb-1">starting at <span class="text-medium">$'+cardData.productPrice+'.00</span></div>',
+                '</div><a class="btn btn-primary scale-up delay-1" href="shop-grid-ls.html">View Offers&nbsp;<i class="icon-arrow-right"></i></a>',
+              '</div>',
+              '<div class="col-md-6 padding-bottom-2x mb-3"><img class="d-block mx-auto" src="img/hero-slider/02.png" alt="Puma Backpack"></div>',
+            '</div>',
+          '</div>',
+        '</div>'
+	];
+	return $(cardFeaturedTemplate.join(''));
+}
